d159f289e8f5abb8dd9774d5b344943047f23b7c
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-05-03 15:05:54 +0200

    Show visual feedback

--- music.exs
+++ music.exs
@@ -21,6 +21,16 @@ defmodule Midi do
     # Immediately reschedule the next tick to reduce drift
     Process.send_after(:midi, {:tick}, state.tick_period)
     current_tick = Map.fetch!(state, :current_tick) + 1
+    show_visual_feedback(current_tick)
     {:noreply, %{state | current_tick: current_tick, tick_period: 50}}
+  end
+  
+  def show_visual_feedback(current_tick) do
+    if rem(current_tick, 64) == 0 do
+      IO.write IO.ANSI.clear <> IO.ANSI.home
+    end
+    if rem(current_tick, 8) == 0 do
+      IO.write IO.ANSI.yellow <> to_string(1 + round(rem(current_tick, 64) / 8)) <> IO.ANSI.default_color
+    end
   end
 end
